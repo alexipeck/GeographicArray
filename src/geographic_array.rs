560adc840a67d576b845f09d45e327e6b791a910
Alexi Peck <alexinicolaspeck@gmail.com>
2022-01-25 17:04:11 +1100

    Managed search starter stuff

--- src/geographic_array.rs
+++ src/geographic_array.rs
@@ -1,5 +1,3 @@
-use rand::seq::index::IndexVec;
-
 use crate::{Vector, CUMULATIVE_DISTANCE_THRESHOLD, IndexVector, coordinate_to_index_x, coordinate_to_index_y, coordinate_to_index_z};
 
 use {
@@ -16,32 +14,32 @@ pub enum Axis {
 
 pub struct GeographicArray {
     pub x: Vec<Vec<ReferenceVector>>,
-    x_median_index: usize,
+    _x_median_index: usize,
     pub y: Vec<Vec<ReferenceVector>>,
-    y_median_index: usize,
+    _y_median_index: usize,
     pub z: Vec<Vec<ReferenceVector>>,
-    z_median_index: usize,
+    _z_median_index: usize,
 }
 
 impl GeographicArray {
     pub fn new(zones: usize) -> Self {
         Self {
             x: vec![Vec::new(); zones],
-            x_median_index: zones / 2,
+            _x_median_index: zones / 2,
             y: vec![Vec::new(); zones],
-            y_median_index: zones / 2,
+            _y_median_index: zones / 2,
             z: vec![Vec::new(); zones],
-            z_median_index: zones / 2,
+            _z_median_index: zones / 2,
         }
     }
     pub fn default() -> Self {
         Self {
             x: vec![Vec::new(); ZONES_USIZE],
-            x_median_index: ZONES_USIZE / 2,
+            _x_median_index: ZONES_USIZE / 2,
             y: vec![Vec::new(); ZONES_USIZE],
-            y_median_index: ZONES_USIZE / 2,
+            _y_median_index: ZONES_USIZE / 2,
             z: vec![Vec::new(); ZONES_USIZE],
-            z_median_index: ZONES_USIZE / 2,
+            _z_median_index: ZONES_USIZE / 2,
         }
     }
 
@@ -60,13 +58,44 @@ impl GeographicArray {
         self.y[y_normalised_index].push(ReferenceVector::new_real_y(
             x_ref.clone(),
             vector.y,
-            z_ref.clone(),
+            z_ref,
         ));
         self.z[z_normalised_index].push(ReferenceVector::new_real_z(
-            x_ref.clone(),
-            y_ref.clone(),
+            x_ref,
+            y_ref,
             vector.z,
         ));
+    }
+
+    fn managed_search(
+        &self,
+        candidates: &mut BTreeMap<OrderedFloat<f64>, ReferenceVector>,
+        deviation_count: &usize,
+        limiter_threshold: &usize,
+        axis: &Axis,
+        search_index: usize,
+        nearest_to: &Vector,
+    ) {
+        if deviation_count <= limiter_threshold {
+            let potential_candidates: Vec<ReferenceVector> = match axis {
+                Axis::X => self.x[search_index].clone(),
+                Axis::Y => self.y[search_index].clone(),
+                Axis::Z => self.z[search_index].clone(),
+            };
+            for reference_vector in potential_candidates.iter() {
+                let cumulative_diff: f64 = reference_vector.calculate_cumulative_diff(nearest_to);
+                if cumulative_diff <= CUMULATIVE_DISTANCE_THRESHOLD {
+                    candidates.insert(OrderedFloat(cumulative_diff), reference_vector.to_real());
+                }
+            }
+        } else {
+            let length = match axis {
+                Axis::X => self.x[search_index].len(),
+                Axis::Y => self.y[search_index].len(),
+                Axis::Z => self.z[search_index].len(),
+            };
+            println!("{} ignored values.", length);
+        }
     }
 
     pub fn find_nearest(
@@ -74,18 +103,6 @@ impl GeographicArray {
         nearest_to: &Vector,
         deviation_limiter_radius_meters: Option<&Vector>,
     ) -> BTreeMap<OrderedFloat<f64>, ReferenceVector> {
-        fn handle_candidates(
-            potential_candidates: Vec<ReferenceVector>,
-            nearest_to: &Vector,
-            candidate: &mut BTreeMap<OrderedFloat<f64>, ReferenceVector>,
-        ) {
-            for g in potential_candidates.iter() {
-                let cumulative_diff: f64 = g.calculate_cumulative_diff(&nearest_to);
-                if cumulative_diff <= CUMULATIVE_DISTANCE_THRESHOLD {
-                    candidate.insert(OrderedFloat(cumulative_diff), g.to_real());
-                }
-            }
-        }
 
 
         //limiter counter currently can't deal with differentiating x, y, z distances, z is the most likely to fuck it up
@@ -104,74 +121,32 @@ impl GeographicArray {
         let mut deviation_count: usize = 0;
         let index_vector: IndexVector = IndexVector::from_vector(nearest_to);
         let limiter_counter_largest_bound = limiter_threshold.max_index();
-        while candidates.is_empty() && deviation_count > limiter_counter_largest_bound {
+        
+        let axis_x = &Axis::X;
+        let axis_y = &Axis::Y;
+        let axis_z = &Axis::Z;
+
+        //still doesn't deal with what happens when it gets to either end of the vector, it should just stop the search on that axis
+        //I need to check how good it is at knowing how far it can actually move when it's near the edge, it might actually be inherantly handled
+        while candidates.is_empty() && deviation_count < limiter_counter_largest_bound {
             if deviation_count == 0 {
-                if deviation_count <= limiter_threshold.x {
-                    handle_candidates(
-                        self.x[index_vector.x].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
-                if deviation_count <= limiter_threshold.y {
-                    handle_candidates(
-                        self.y[index_vector.y].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
-
-                if deviation_count <= limiter_threshold.z {
-                    handle_candidates(
-                        self.z[index_vector.z].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
+                //xyz
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.x, axis_x, index_vector.x, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.y, axis_y, index_vector.y, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.z, axis_z, index_vector.z, nearest_to);
             } else {
-                //x
-                if deviation_count <= limiter_threshold.x {
-                    handle_candidates(self.x[index_vector.x + deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                    handle_candidates(
-                        self.x[index_vector.x - deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
-
-                //y
-                if deviation_count <= limiter_threshold.y {
-                    handle_candidates(
-                        self.y[index_vector.y + deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                    handle_candidates(
-                        self.y[index_vector.y - deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
-
-                //z
-                if deviation_count <= limiter_threshold.z {
-                    handle_candidates(
-                        self.z[index_vector.z + deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                    handle_candidates(
-                        self.z[index_vector.z - deviation_count].clone(),
-                        nearest_to,
-                        &mut candidates,
-                    );
-                }
+                //+- deviation_count
+                //xxyyzz
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.x, axis_x, index_vector.x + deviation_count, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.x, axis_x, index_vector.x - deviation_count, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.y, axis_y, index_vector.y + deviation_count, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.y, axis_y, index_vector.y - deviation_count, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.z, axis_z, index_vector.z + deviation_count, nearest_to);
+                self.managed_search(&mut candidates, &deviation_count, &limiter_threshold.z, axis_z, index_vector.z - deviation_count, nearest_to);
             }
             deviation_count += 1;
         }
+        println!("Found {} candidates.", candidates.len());
         candidates
     }
 
@@ -200,6 +175,8 @@ impl GeographicArray {
             synthetic_value.y,
             synthetic_value.z,
         );
+
+        //move to testing.rs
         let near_candidates = self.find_nearest(&synthetic_value, None);
         for (cumulative_distance, coordinate) in near_candidates {
             println!(
@@ -225,7 +202,7 @@ impl GeographicArray {
 
             let ordered_candidates = self.find_nearest(&random_vector, Some(&Vector::new(1000.0, 1000.0, 1000.0)));
 
-            if !ordered_candidates.is_empty() {
+            for (cumulative_distance, reference_vector) in ordered_candidates.iter() {
                 println!(
                     "{}: Nearest to random value: X: {}, Y: {}, Z: {}",
                     start_time.elapsed().as_micros(),
@@ -233,18 +210,17 @@ impl GeographicArray {
                     random_vector.y,
                     random_vector.z
                 );
-                for (cumulative_distance, g) in ordered_candidates {
-                    println!(
-                        "{}: Distance: {:17}, X: {}, Y: {}, Z: {}",
-                        start_time.elapsed().as_micros(),
-                        cumulative_distance,
-                        g.x(),
-                        g.y(),
-                        g.z()
-                    );
-                    break; //Only want to run once, she'll be right
-                }
-            } else {
+                println!(
+                    "{}: Distance: {:17}, X: {}, Y: {}, Z: {}",
+                    start_time.elapsed().as_micros(),
+                    cumulative_distance,
+                    reference_vector.x(),
+                    reference_vector.y(),
+                    reference_vector.z()
+                );
+            }
+
+            if ordered_candidates.is_empty() {
                 println!(
                     "{}: Couldn't find a value within threshold",
                     start_time.elapsed().as_micros()
